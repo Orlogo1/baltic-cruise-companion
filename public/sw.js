d626a8e53d6b1d50161a8ba7abfcdbae160b3f6e
Orlogo1 <odd.devlin@gmail.com>
2026-08-24 08:22:02 -0400

    Refresh offline cache for London update

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "baltic-companion-github-v4";
+const CACHE = "baltic-companion-github-v5-london";
 const BASE = "/baltic-cruise-companion/";
 const SHELL = [BASE, `${BASE}favicon.svg`, `${BASE}site.webmanifest`];
 
